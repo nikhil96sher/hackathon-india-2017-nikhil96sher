cd6005e9be85f990cf50a6f3c8ffb6517232a246
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2017-10-07 17:11:26 +0530

    Added Basic Template

--- roads/rewardingroads/urls.py
+++ roads/rewardingroads/urls.py
@@ -13,12 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
-from rewardingroads import urls as roads_urls 
+from django.conf.urls import url 
+from rewardingroads.views import *
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^roads/', include('rewardingroads.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'$', index),
+]
